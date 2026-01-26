13dfdd727af3ce2521804a3a7c5df7f1d4098740
thanksalone <thanksalone01@gmail.com>
2026-01-26 22:29:54 +0500

    117

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import "./styles/app.scss";
+import "./styles/dpp.scss";
 
 import App from './App.tsx'
 import { Provider } from 'react-redux';
